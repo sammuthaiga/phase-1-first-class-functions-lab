4b67a8680e3cd91c37556208820a38578aa69353
Samuel <samuel.muthaiga@student.moringaschool.com>
2022-11-25 17:58:18 +0300

    commit tree 264407b0b6a91d6e9c8fc375e4c43de2d3b437bc

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    let newArray = drivers.slice(0,2);
+    return newArray;
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = function(){
+    //should return an array of the last two drivers
+    let newArray = drivers.slice(-2);
+    return newArray;
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+    return function (fareMultiplier) {
+        return fareMultiplier * number
+    }
+}
+const fareDoubler = function(fareMultiplier){
+    return createFareMultiplier(2)(fareMultiplier)
+}
+const fareTripler = function(fareMultiplier){
+    return createFareMultiplier(3)(fareMultiplier)
+}
+function selectDifferentDrivers(arrayOfDrivers, drivers) {
+    return drivers(arrayOfDrivers)
+}
+
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers);
+selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers);
